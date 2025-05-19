9c982b0c6e0c268881602bbe92883baae64ea44d
Болотов Никита Сергеевич <bolotovN@fizpribor.ru>
2025-05-19 07:08:43 +0300

    Add resources folder

--- usage-example.lisp
+++ usage-example.lisp
@@ -3,12 +3,12 @@
 (defun hello-form (params)
   (if params
       (send-data '(200 . text/html) (format nil "<html>Hello ~a</html>" (cdr (assoc 'fname params))))
-      (send-file "hello.html")))
+      (send-file "resources/hello.html")))
 
 (defun req-handler (path params)
-  (cond ((equal path "data") (send-data '(200 . application/json) "{\"name\": \"Nikita\", \"age\": 21}"))
+  (cond ((equal path "/data") (send-data '(200 . application/json) "{\"name\": \"Nikita\", \"age\": 21}"))
         ((equal path "/hello") (hello-form params))
-        (t (send-file (subseq path 1)))))
+        (t (send-file (format nil "resources/~a" (subseq path 1))))))
 
 (defun main ()
   (serv 0 #'req-handler))
